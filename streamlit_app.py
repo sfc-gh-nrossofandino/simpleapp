050078d8e15e3ea1078ab4a9524f6204649a3d7c
Emiliano Rosso <arraydude@gmail.com>
2020-08-14 18:22:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,3 @@
 import streamlit as st
 
 st.title("Test App!!!")
-
-file_obj = st.sidebar.file_uploader('Choose an image:', ('jpg', 'jpeg'))
-
-value = st.slider("Pick a number", 0, 10, 3)
-
-st.write(value)
-
-input = st.text_input("Tell me something", "Cantami o Diva")
-
-with open("temp_file.txt", "a") as f:
-  f.write(f"{input}\n")
-  
-with open("temp_file.txt", "r") as f:
-  st.write(f.read())
-
-st.write("Streamlit is fabulous")
-
-st.write("Hello Corey, this is the demo!")
-st.balloons()
-print("this is a log line")
