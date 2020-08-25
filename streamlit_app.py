b799ed22e81819fccf70572488448e4ebd06fc68
arraydude <arraydude@gmail.com>
2020-08-25 19:20:10 -0300

    log colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,21 @@ import streamlit as st
 import coloredlogs, logging
 
 # Create a logger object.
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 # By default the install() function installs a handler on the root logger,
 # this means that log messages from your code and log messages from the
 # libraries that you use will all show up on the terminal.
-coloredlogs.install(level='DEBUG')
+#coloredlogs.install(level='DEBUG')
 
 # If you don't want to see log messages from libraries, you can pass a
 # specific logger object to the install() function. In this case only log
 # messages originating from that logger will show up on the terminal.
-coloredlogs.install(level='DEBUG', logger=logger)
+#coloredlogs.install(level='DEBUG', logger=logger)
 
 st.title("Test App!!!")
 
 log_text = st.text_input('Log text:')
 if st.button("send log"):
-    logger.info(log_text)
+    logging.info(log_text)
 
